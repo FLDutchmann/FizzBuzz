02a0f933da3ecadb160a977f413fdb4a3fe43173
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-06-29 15:47:29 +0100

    Refactored the basic add word rules

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,24 +5,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FizzBuzz {
-
     public static void main(String[] args) {
-
         for(int i = 1; i <= 300; i++) {
+            List<BasicRule> basicRules = new ArrayList<>();
+            basicRules.add(new BasicRule(3, "Fizz"));
+            basicRules.add(new BasicRule(13, "Fezz"));
+            basicRules.add(new BasicRule(5, "Buzz"));
+            basicRules.add(new BasicRule(7, "Bang"));
+
             List<String> words = new ArrayList<>();
 
-            if(i % 3 == 0) words.add("Fizz");
-
-            if(i % 13 == 0) words.add("Fezz");
-
-            if(i % 5 == 0) words.add("Buzz");
-
-            if(i % 7 == 0) words.add("Bang");
+            for(BasicRule rule: basicRules) {
+                addWordIfDivisible(i, rule.divisor, rule.word, words);
+            }
 
             if(i % 11 == 0) {
                 words.add("Bong");
                 words = words.stream()
-                             .filter((word)-> word != "Fizz" && word != "Buzz" && word != "Bang")
+                             .filter((word)-> !word.equals("Fizz") && !word.equals("Buzz") && !word.equals("Bang"))
                              .collect(Collectors.toList());
             }
 
@@ -36,9 +36,19 @@ public class FizzBuzz {
             }
             System.out.println();
         }
-
-
-
     }
 
+    public static void addWordIfDivisible(int iterator, int divisor, String word, List<String> words){
+        if(iterator % divisor == 0) words.add(word);
+    }
+
+    private static class BasicRule {
+        public int divisor;
+        public String word;
+
+        BasicRule(int divisor, String word) {
+            this.divisor = divisor;
+            this.word = word;
+        }
+    }
 }
