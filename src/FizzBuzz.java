8b3e30a09eee7c4b87f68e84c296256a6e48d69e
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-06-29 13:34:56 +0100

    Part 1 FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class FizzBuzz {
+import java.util.LinkedList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++) {
+            List<String> words = new LinkedList<>();
+
+            if(i % 3 == 0) words.add("Fizz");
+            if(i % 5 == 0) words.add("Buzz");
+
+            if(words.isEmpty()) {
+                System.out.print(i);
+            } else {
+                for(String word: words) System.out.print(word);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
 }
